115be951f60294871939ff1f50ffa29f4772009b
student <student@mitso.by>
2023-12-14 18:46:35 +0300

    4

--- src/bloom-filter.js
+++ src/bloom-filter.js
@@ -1,139 +1,63 @@
 const { NotImplementedError } = require("../extensions/index.js"); 
  
-// const { Node } = require('../extensions/list-tree.js'); 
+class BloomFilter { 
+  constructor(size = 99) { 
+    this.store = this.createStore(size); 
+  } 
  
-/** 
- * Implement simple binary search tree according to task description 
- * using Node from extensions 
- */ 
-class Node { 
-  constructor(data) { 
-    this.data = data; 
-    this.left = null; 
-    this.right = null; 
+  insert(item) { 
+    const hashValues = this.getHashValues(item); 
+    hashValues.forEach((hash) => { 
+      this.store.setValue(hash, true); 
+    }); 
+  } 
+ 
+  mayContain(item) { 
+    const hashValues = this.getHashValues(item); 
+    return hashValues.every((hash) => this.store.getValue(hash)); 
+  } 
+ 
+  createStore(size) { 
+    return { 
+      store: Array(size).fill(false), 
+      getValue: function (index) { 
+        return this.store[index]; 
+      }, 
+      setValue: function (index, value) { 
+        this.store[index] = value; 
+      }, 
+    }; 
+  } 
+ 
+  hash(item, seed) { 
+    let hash = seed; 
+    for (let i = 0; i < item.length; i++) { 
+      const char = item.charCodeAt(i); 
+      hash = ((hash * 31+char) % 1000000000); 
+    } 
+    return hash % 100; 
+  } 
+ 
+ 
+  hash1(item) { 
+    return (this.hash(item, -1) % 100) + 3; 
+  } 
+ 
+  hash2(item) { 
+    return this.hash(item, -1) % 100; 
+  } 
+ 
+  hash3(item) { 
+    return this.hash(item, 0) % 100; 
+  } 
+ 
+  getHashValues(item) { 
+    return [ 
+      this.hash1(item), 
+      this.hash2(item), 
+      this.hash3(item) 
+    ]; 
   } 
 } 
  
-module.exports = class BinarySearchTree { 
-  constructor() { 
-    this.treeRoot = null; 
-  } 
- 
-  root() { 
-    return this.treeRoot; 
-  } 
- 
-  add(data) { 
-    const newNode = new Node(data); 
- 
-    if (this.treeRoot === null) { 
-      this.treeRoot = newNode; 
-    } else { 
-      this.insertNode(this.treeRoot, newNode); 
-    } 
-  } 
- 
-  insertNode(node, newNode) { 
-    if (newNode.data < node.data) { 
-      if (node.left === null) { 
-        node.left = newNode; 
-      } else { 
-        this.insertNode(node.left, newNode); 
-      } 
-    } else { 
-      if (node.right === null) { 
-        node.right = newNode; 
-      } else { 
-        this.insertNode(node.right, newNode); 
-      } 
-    } 
-  } 
- 
-  has(data) { 
-    return this.searchNode(this.treeRoot, data) !== null; 
-  } 
- 
-  find(data) { 
-    return this.searchNode(this.treeRoot, data); 
-  } 
- 
-  searchNode(node, data) { 
-    if (node === null) { 
-      return null; 
-    } else if (data < node.data) { 
-      return this.searchNode(node.left, data); 
-    } else if (data > node.data) { 
-      return this.searchNode(node.right, data); 
-    } else { 
-      return node; 
-    } 
-  } 
- 
-  remove(data) { 
-    this.treeRoot = this.removeNode(this.treeRoot, data); 
-  } 
- 
-  removeNode(node, key) { 
-    if (node === null) { 
-      return null; 
-    } else if (key < node.data) { 
-      node.left = this.removeNode(node.left, key); 
-      return node; 
-    } else if (key > node.data) { 
-      node.right = this.removeNode(node.right, key); 
-      return node; 
-    } else { 
-      if (node.left === null && node.right === null) { 
-        node = null; 
-        return node; 
-      } 
- 
-      if (node.left === null) { 
-        node = node.right; 
-        return node; 
-      } 
- 
-      if (node.right === null) { 
-        node = node.left; 
-        return node; 
-      } 
- 
-      const minNode = this.findMinNode(node.right); 
-      node.data = minNode.data; 
-      node.right = this.removeNode(node.right, minNode.data); 
-      return node; 
-    } 
-  } 
- 
-  findMinNode(node) { 
-    if (node.left === null) { 
-      return node; 
-    } else { 
-      return this.findMinNode(node.left); 
-    } 
-  } 
- 
-  min() { 
-    if (this.treeRoot === null) { 
-      return null; 
-    } 
- 
-    let currentNode = this.treeRoot; 
-    while (currentNode.left !== null) { 
-      currentNode = currentNode.left; 
-    } 
-    return currentNode.data; 
-  } 
- 
-  max() { 
-    if (this.treeRoot === null) { 
-      return null; 
-    } 
- 
-    let currentNode = this.treeRoot; 
-    while (currentNode.right !== null) { 
-      currentNode = currentNode.right; 
-    } 
-    return currentNode.data; 
-  } 
-};
+module.exports = BloomFilter;
